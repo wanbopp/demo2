8dff1d64c0988683971897c29f7bbfd1b98e1dbb
panwanbo <wanbopp@163.com>
2023-10-24 09:18:56 +0800

    121.买股票的最大利润1

--- leetCode/src/main/java/wanbo/com/suanfa/Solution.java
+++ leetCode/src/main/java/wanbo/com/suanfa/Solution.java
@@ -379,17 +379,18 @@ public class Solution {
 
     }
 
-    //P2.数组翻转
+    //P3.数组翻转
     public void rotate2(int[] nums, int k) {
         // nums = [1,2,3,4,5,6,7]
         k = k % nums.length;
         //1、先翻转所有元素
-        reverse(nums, 0, nums.length-1);
+        reverse(nums, 0, nums.length - 1);
         //2、分开两个区间、需要移动的元素  和   原始元素同时进行翻转
         reverse(nums, 0, k - 1);
         reverse(nums, k, nums.length - 1);
         System.out.println("nums = " + Arrays.toString(nums));
     }
+
     public void reverse(int[] nums, int start, int end) {
         while (start < end) {
             int temp = nums[start];
@@ -401,9 +402,84 @@ public class Solution {
     }
 
 
-    public static void main(String[] args) {
-        int[] nums = {1, 2, 3, 4, 5, 6, 7};
-        new Solution().rotate2(nums, 4);
+//    public static void main(String[] args) {
+//        int[] nums = {1, 2, 3, 4, 5, 6, 7};
+//        new Solution().rotate2(nums, 4);
+//    }
+
+    /**
+     * 121.买股票的最佳时机
+     * 给定一个数组 prices ，它的第 i 个元素 prices[i] 表示一支给定股票第 i 天的价格。
+     * 你只能选择 某一天 买入这只股票，并选择在 未来的某一个不同的日子 卖出该股票。设计一个算法来计算你所能获取的最大利润。
+     * 返回你可以从这笔交易中获取的最大利润。如果你不能获取任何利润，返回 0
+     * <p>
+     * 示例 1：
+     * <p>
+     * 输入：[7,1,5,3,6,4]
+     * 输出：5
+     * 解释：在第 2 天（股票价格 = 1）的时候买入，在第 5 天（股票价格 = 6）的时候卖出，最大利润 = 6-1 = 5 。
+     * 注意利润不能是 7-1 = 6, 因为卖出价格需要大于买入价格；同时，你不能在买入前卖出股票。
+     * 示例 2：
+     * <p>
+     * 输入：prices = [7,6,4,3,1]
+     * 输出：0
+     * 解释：在这种情况下, 没有交易完成, 所以最大利润为 0
+     *
+     * @param prices
+     * @return
+     */
+    //P1 找到差额最大值
+    public int maxProfit(int[] prices) {
+        //一次遍历，同时记录过往价格最小值和最大利润
+        //遍历时 最小值与当前值作比较、最大利润与当前利润比较
+        int minPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+        for (int price : prices) {
+            if (price < minPrice) {
+                minPrice = price;
+            } else if (price - minPrice > maxProfit) {
+                maxProfit = price - minPrice;
+            }
+        }
+        return maxProfit;
+
     }
 
+//    public static void main(String[] args) {
+//        int[] prices = {7, 1, 5, 3, 6, 4};
+//        int i = new Solution().maxProfit(prices);
+//        System.out.println("i = " + i);
+//
+//    }
+
+    /**
+     * 121.买股票的最佳时机 II
+     * 给你一个整数数组 prices ，其中 prices[i] 表示某支股票第 i 天的价格
+     * 在每一天，你可以决定是否购买和/或出售股票。你在任何时候 最多 只能持有 一股 股票。你也可以先购买，然后在 同一天 出售
+     * 返回 你能获得的 最大 利润 。
+     * <p>
+     * <p>
+     * <p>
+     * 示例 1：
+     * 输入：prices = [7,1,5,3,6,4]
+     * 输出：7
+     * 解释：在第 2 天（股票价格 = 1）的时候买入，在第 3 天（股票价格 = 5）的时候卖出, 这笔交易所能获得利润 = 5 - 1 = 4
+     * 随后，在第 4 天（股票价格 = 3）的时候买入，在第 5 天（股票价格 = 6）的时候卖出, 这笔交易所能获得利润 = 6 - 3 = 3
+     * 总利润为 4 + 3 = 7
+     * <p>
+     * 示例 2：
+     * 输入：prices = [1,2,3,4,5]
+     * 输出：4
+     * 解释：在第 1 天（股票价格 = 1）的时候买入，在第 5 天 （股票价格 = 5）的时候卖出, 这笔交易所能获得利润 = 5 - 1 = 4
+     * 总利润为 4
+     * <p>
+     * 示例 3：
+     * 输入：prices = [7,6,4,3,1]
+     * 输出：0
+     * 解释：在这种情况下, 交易无法获得正利润，所以不参与交易可以获得最大利润，最大利润为 0
+     */
+
+//    public int maxProfit2(int[] prices) {
+//
+//    }
 }
