6e0a1794d2890032d9e22f495a3821e25810e000
wanbopp <panwanbo@sinosoft.com.cn>
2024-06-20 13:17:40 +0800

    205. 同构字符串

--- leetCode/src/main/java/wanbo/com/suanfa/Solution.java
+++ leetCode/src/main/java/wanbo/com/suanfa/Solution.java
@@ -2577,9 +2577,38 @@ public class Solution {
 
     }
 
+    /**
+     * 205. 同构字符串
+     * 给定两个字符串 s 和 t ,判断他们是否是同构的
+     * 如果S 中的字符可以按某种映射关系替换得到t,那么两个字符串是同构的。
+     * 每个出现的字符都应当映射到另一个字符，同时不改变字符的顺序
+     * 不同字符不能映射到同一个字符上，相同字符只能映射到同一个字符上，字符可以映射到自己本身
+     *
+     * @param s
+     * @param t
+     * @return
+     */
+    public boolean isIsomorphic(String s, String t) {
+        Map<Character, Character> s2t = new HashMap<Character, Character>();
+        Map<Character, Character> t2s = new HashMap<Character, Character>();
+        //需要维护双向映射关系
+        int len = s.length();
+        for (int i = 0; i < len; ++i) {
+            char x = s.charAt(i), y = t.charAt(i);
+            if ((s2t.containsKey(x) && s2t.get(x) != y) || (t2s.containsKey(y) && t2s.get(y) != x)) {
+                return false;
+            }
+            s2t.put(x, y);
+            t2s.put(y, x);
+        }
+        return true;
+    }
+
+
+
     public static void main(String[] args) {
         Solution solution = new Solution();
-        boolean b = solution.canConstruct("aa", "aab");
+        boolean b = solution.isIsomorphic("egg", "add");
     }
 }
 //    public static void main(String[] args) {
