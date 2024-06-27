051b12f041deb9b318effc1ce013f1963fd7e0cb
wanbopp <panwanbo@sinosoft.com.cn>
2024-06-27 10:06:25 +0800

    1.两数只和

--- leetCode/src/main/java/wanbo/com/suanfa/Solution.java
+++ leetCode/src/main/java/wanbo/com/suanfa/Solution.java
@@ -2775,9 +2775,49 @@ public class Solution {
 
     }
 
+    /**
+     * 1.两数只和
+     * 给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 target  的那 两个 整数，并返回它们的数组下标。
+     * 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素在答案里不能重复出现。
+     * 你可以按任意顺序返回答案。
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int[] twoSum2(int[] nums, int target) {
+//        //TODO P1 返回的是下标没办法排序
+//        //考虑差值数组
+//        for (int i = 0; i < nums.length; i++) {
+//            for (int j = 0; j < nums.length; j++) {
+//                if (i == j) {
+//                    continue;
+//                }
+//                if (target - nums[i] == nums[j]) {
+//                    return new int[]{i, j};
+//                }
+//            }
+//        }
+//        return null;
+
+        //TODO P2 hash表保存遍历过的差值和他的下标
+        //一次遍历，一般情况下，空间和时间是需要二选一的
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                return new int[]{i, map.get(target - nums[i])};
+            } else {
+                map.put(nums[i], i);
+            }
+
+        }
+
+        return null;
+    }
+
     public static void main(String[] args) {
         Solution solution = new Solution();
-        boolean b = solution.isAnagram("anagram", "nagaram");
+        int[] b = solution.twoSum2(new int[]{2, 7, 11, 15}, 9);
     }
 }
 //    public static void main(String[] args) {
